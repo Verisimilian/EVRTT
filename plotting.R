da81a649ffe75f4e3bca733504a5c84ad92b2795
Ryan Musich <30703345+rjmusich@users.noreply.github.com>
2018-01-27 19:29:19 -0500

    Add files via upload

--- plotting.R
+++ plotting.R
@@ -1,19 +1,27 @@
-r = paste0(df[1,1], df[1,2])
-m = paste(df[1,1], df[1,2])
-a = data.frame(Hits = 1, Date = m, row.names = r)
-for(i in 2:nrow(df)){
-  tmp = paste0(df[i,1], df[i,2])
-  m = paste(df[i, 1], df[i, 2])
-  if(tmp %in% rownames(a)){
-    a[tmp, 1] = a[tmp, 1] + 1
+
+
+first = df[1,]
+last = df[nrow(df),]
+ind = match(first[1,1], month.abb)
+fyr = first[1,2]
+lyr = last[1,2]
+a = as.data.frame(matrix(nrow = 0, ncol = 2), col.names = c('Hits', 'Date'))
+for(i in fyr:lyr){
+  for(j in ind:12){
+    r = paste0(month.abb[j], i)
+    m = paste(month.abb[j], i)
+    tmp = data.frame(Hits = 0, Date = m, row.names = r)
+    a = rbind(a, tmp)
   }
-  else{
-    x = data.frame(Hits = 1, Date = m, row.names = tmp)
-    a = rbind(a, x)
-  }
+  j = 1
 }
-d = 1:nrow(a)
-a = cbind(a, d)
+
+
+for(i in 1:nrow(df)){
+  tmp = paste0(df[i,1], df[i,2])
+  a[tmp, 1] = a[tmp, 1] + 1
+}
+
 a$Date = as.yearmon(a$Date)
 plot(a$Date, a$Hits, type = 'l', xaxt = 'n')
 axis(1, a$Date, format(a$Date, "%b %Y"))
